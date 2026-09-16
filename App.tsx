8ace522f3c86d32ff2161fcbb7d75fc43b924409
fblinn <fatimablinares@gmail.com>
2026-09-15 18:46:38 -0600

    Cambio para el asycnstorage

--- App.tsx
+++ App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { store } from '@/redux/store';
+import { store, persistor } from '@/redux/store';
 
 import AppNavigator from '@/navigation/AppNavigator';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <AppNavigator />
-        <StatusBar style="light" />
-      </SafeAreaProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <SafeAreaProvider>
+          <AppNavigator />
+          <StatusBar style="light" />
+        </SafeAreaProvider>
+      </PersistGate>
     </Provider>
   );
 }
